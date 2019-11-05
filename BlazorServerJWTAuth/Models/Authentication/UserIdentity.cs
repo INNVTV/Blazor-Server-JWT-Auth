281e641363034dee4f4a423c2514011bd9a55a52
Kaz <kaz@innvtv.com>
2019-11-04 17:49:25 -0800

    Initial commit

---
+++ BlazorServerJWTAuth/Models/Authentication/UserIdentity.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorServerJWTAuth.Models.Authentication
+{
+    public class UserIdentity
+    {
+        public UserIdentity()
+        {
+            IsAuthenticated = false;
+            Roles = new List<string>();
+        }
+
+        public string Id { get; set; }
+        public string UserName { get; set; }
+        public string Email { get; set; }
+        public bool IsAuthenticated { get; set; }
+        public List<string> Roles { get; set; }
+
+
+    }
+}
